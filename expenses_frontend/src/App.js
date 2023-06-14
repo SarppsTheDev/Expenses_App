24a64d2dd74ba6a3a84425ff3a696e6f7a72d27e
Michael <michael.sarpong-boateng@flagstoneim.com>
2023-06-14 17:21:47 +0100

    Front end app with Add Expense error

---
+++ expenses_frontend/src/App.js
@@ -0,0 +1,13 @@
+import ExpenseForm from "./components/expenseForm";
+import ExpenseList from "./components/expenseList";
+
+const App = () => (
+  <div style={{ width: '60%', margin: 'auto' }}>
+    <h3>New Expense</h3>
+    <ExpenseForm />
+    <hr style = {{border: '1px solid grey'}} />
+    <h3>Your Expenses</h3>
+    <ExpenseList />
+  </div>
+)
+export default App;
