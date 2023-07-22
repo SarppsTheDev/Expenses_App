b63dbcbd13dcf4c745b2cb970c5b366ae96c3c45
Michael Sarpong-Boateng <info@sarppsthedev.com>
2023-07-22 18:35:15 +0100

    Additional API operations

--- expenses_backend/expenses_core/ExpensesServices.cs
+++ expenses_backend/expenses_core/ExpensesServices.cs
@@ -20,4 +20,28 @@ public class ExpensesServices : IExpensesServices
     {
         return _context.Expenses.ToList();
     }
+
+    public Expense CreateExpense(Expense expense)
+    {
+        _context.Add(expense);
+        _context.SaveChanges();
+
+        return expense;
+    }
+
+    public void DeleteExpense(Expense expense)
+    {
+        _context.Expenses.Remove(expense);
+        _context.SaveChanges();
+    }
+
+    public Expense EditExpense(Expense expense)
+    {
+        var dbExpense = _context.Expenses.First(e => e.Id == expense.Id);
+        dbExpense.Description = expense.Description;
+        dbExpense.Amount = expense.Amount;
+        _context.SaveChanges();
+
+        return dbExpense;
+    }
 }
