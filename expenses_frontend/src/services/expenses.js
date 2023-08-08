96ad603c71d65962486252ee0f5c194fdc63b034
Michael Sarpong-Boateng <info@sarppsthedev.com>
2023-08-08 19:21:52 +0100

    Refactoring to use slice instead reducer

--- expenses_frontend/src/services/expenses.js
+++ expenses_frontend/src/services/expenses.js
@@ -1,49 +1,50 @@
-import { ActionCreators } from "../app/expensesReducer";
-import axios from 'axios';
+import { setExpenses, newExpense, editExpense, deleteExpense } from "../app/expensesSlice";
+import axios from "axios";
 
 const axiosInstance = axios.create({
-    baseURL: 'http://localhost:5285/Expenses',
-    headers: {
-        'Access-Control-Allow-Origin': '*'
-    }
-})
+  baseURL: "http://localhost:5285/Expenses",
+  headers: {
+    "Access-Control-Allow-Origin": "*",
+  },
+});
 
 export const GetExpenses = async (dispatch) => {
-    try {
-        // api call
-        const { data } = await axiosInstance.get();
-        dispatch(ActionCreators.setExpenses(data));
-    } catch {
-        console.log('Error!')
-    }
-}
+  try {
+    // api call
+    const { data } = await axiosInstance.get();
+    dispatch(setExpenses(data));
+  } catch {
+    console.log("Error!");
+  }
+};
 
 export const NewExpense = async (dispatch, expense) => {
-    try {
-        // api call
-        const { data } = await axiosInstance.post('', expense);
-        dispatch(ActionCreators.newExpense(data));
-    } catch {
-        console.log('Error!')
-    }
-}
+  try {
+    // api call
+    const { data } = await axiosInstance.post("", expense);
+    dispatch(newExpense(data));
+  } catch {
+    console.log("Error!");
+  }
+};
 
 export const EditExpense = async (dispatch, expense) => {
-    try {
-        // api call
-        const { data } = await axiosInstance.put('', expense);
-        dispatch(ActionCreators.editExpense(expense));
-    } catch {
-        console.log('Error!');
-    }
-}
+  try {
+    // api call
+    const { data } = await axiosInstance.put("", expense);
+    dispatch(editExpense(expense));
+  } catch {
+    console.log("Error!");
+  }
+};
 
 export const DeleteExpense = async (dispatch, expense) => {
-    try {
-        // api call
-        const { data } = await axiosInstance.delete('', expense);
-        dispatch(ActionCreators.deleteExpense(expense));
-    } catch {
-        console.log('Error!');
-    }
-}
+  try {
+    // api call
+    console.log("Deleting expense: ", expense);
+    const { data } = await axiosInstance.delete("", { data: { ...expense } });
+    dispatch(deleteExpense(expense));
+  } catch {
+    console.log("Error!");
+  }
+};
