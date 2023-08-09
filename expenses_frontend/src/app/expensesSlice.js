2cb2a90dafd180f37cd0dc44db37a7810ae4fcdb
Michael Sarpong-Boateng <info@sarppsthedev.com>
2023-08-09 16:39:47 +0100

    Added toast notifications for each action

--- expenses_frontend/src/app/expensesSlice.js
+++ expenses_frontend/src/app/expensesSlice.js
@@ -1,33 +1,40 @@
-import {createSlice} from '@reduxjs/toolkit';
+import { createSlice, createAction } from "@reduxjs/toolkit";
+
+export const setExpensesError = createAction('setExpensesError');
+export const newExpensesError = createAction('newExpensesError');
+export const editExpensesError = createAction('editExpensesError');
+export const deleteExpensesError = createAction('deleteExpensesError');
 
 export const expensesSlice = createSlice({
-    name: 'expenses',
-    initialState: {
-        expenses: [],
+  name: "expenses",
+  initialState: {
+    expenses: [],
+  },
+  reducers: {
+    setExpenses: (state, action) => {
+      return { ...state, expenses: [...action.payload] };
     },
-    reducers: {
-        setExpenses: (state, action) => {
-            return { ...state, expenses: [...action.payload] };
-        },
-        newExpense: (state, action) => {
-            return { ...state, expenses: [...action.payload, ...state.expenses] };
-        },
-        editExpense: (state, action) => {
-            var expenses = state.expenses.map(expense => {
-                if (expense.id === action.payload.id) {
-                    expense = action.payload;
-                }
-                return expense;
-            })
-        },
-        deleteExpense: (state, action) => {
-            var expenses = state.expenses.filter(expense =>
-                expense.id !== action.payload.id);
-            return { ...state, expenses: [...expenses] }
+    newExpense: (state, action) => {
+      return { ...state, expenses: [...action.payload, ...state.expenses] };
+    },
+    editExpense: (state, action) => {
+      var expenses = state.expenses.map((expense) => {
+        if (expense.id === action.payload.id) {
+          expense = action.payload;
         }
-    }
+        return expense;
+      });
+    },
+    deleteExpense: (state, action) => {
+      var expenses = state.expenses.filter(
+        (expense) => expense.id !== action.payload.id
+      );
+      return { ...state, expenses: [...expenses] };
+    },
+  },
 });
 
-export const {setExpenses, newExpense, editExpense, deleteExpense} = expensesSlice.actions;
+export const { setExpenses, newExpense, editExpense, deleteExpense } =
+  expensesSlice.actions;
 
 export default expensesSlice.reducer;
